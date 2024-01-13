9a18c33d5266451ea4357a62ed9bb04d846f060e
Sergio Rubio <HG86revient@hotmail.com>
2024-01-13 11:48:24 -0600

    asideMenu is now a standalone component, modified styles, Calculation component added, validators added, currency pipe implemented, next step is tie non-zero values to enable submit button and display mock data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
+import { AsideMenuComponent } from './components/aside-menu/aside-menu.component';
+import { CalculationComponent } from './components/calculation/calculation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,NavComponent],
+  imports: [CommonModule, RouterOutlet,NavComponent,AsideMenuComponent,CalculationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/calculation/calculation.component.ts
+++ src/app/components/calculation/calculation.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import {  FormControl,ReactiveFormsModule, Validators} from '@angular/forms'
+import { CurrencyPipe } from '@angular/common';
+import { DigitOnlyModule } from '@uiowa/digit-only';
 
 @Component({
   selector: 'app-calculation',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,CurrencyPipe,DigitOnlyModule],
   templateUrl: './calculation.component.html',
   styleUrl: './calculation.component.css'
 })
 export class CalculationComponent {
-
+  mainLimit = new FormControl('0',Validators.min(0))
+  mainRetention = new FormControl('0',Validators.min(0))
+  executionType = new FormControl('Simple',Validators.required)
 }
