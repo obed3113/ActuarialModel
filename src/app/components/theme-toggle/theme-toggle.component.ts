5d1f6bd6e5adc796ed30da6aea50f0afadf632dc
Sergio Rubio <HG86revient@hotmail.com>
2024-01-14 00:43:24 -0600

    theme toggler added, logic works in the whole app TODO: implement additional fullscreen load spinner

--- src/app/components/theme-toggle/theme-toggle.component.ts
+++ src/app/components/theme-toggle/theme-toggle.component.ts
@@ -1,28 +1,39 @@
-import { Component, Renderer2, ElementRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-theme-toggle',
   standalone:true,
   templateUrl: './theme-toggle.component.html',
-  styleUrls: ['./theme-toggle.component.scss']
+  styleUrls: ['./theme-toggle.component.css']
 })
+export class ThemeToggleComponent implements OnInit {
+  theme: string ='light';
 
-// TODO: adaptar la logica del toggler a la logica de angular
+  ngOnInit() {
+    this.theme = this.getInitialTheme();
+    this.applyTheme(this.theme);
+  }
 
+  handleToggleClick() {
+    const isDark = this.theme === 'dark';
+    this.theme = isDark ? 'light' : 'dark';
+    this.applyTheme(this.theme);
+    this.saveThemeToLocalStorage(this.theme);
+  }
 
-export class ThemeToggleComponent {
-  constructor(private renderer: Renderer2, private el: ElementRef) {}
-
-  handleToggleClick(): void {
-    const element = this.el.nativeElement;
-    const isDark = element.classList.contains("dark");
-
-    if (isDark) {
-      this.renderer.removeClass(element, 'dark');
-      localStorage.setItem("theme", "light");
-    } else {
-      this.renderer.addClass(element, 'dark');
-      localStorage.setItem("theme", "dark");
+  private getInitialTheme(): string {
+    if (localStorage.getItem('theme')) {
+      return localStorage.getItem('theme') ?? 'light';
     }
+
+    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+  }
+
+  private applyTheme(theme: string): void {
+    document.documentElement.classList.toggle('dark', theme === 'dark');
+  }
+
+  private saveThemeToLocalStorage(theme: string): void {
+    localStorage.setItem('theme', theme);
   }
 }
