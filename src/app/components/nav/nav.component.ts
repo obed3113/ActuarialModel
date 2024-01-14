2c7acce9abe462c33f363ed1b959aeb3fcde2efb
Sergio Rubio <HG86revient@hotmail.com>
2024-01-14 03:01:05 -0600

    wip: adapting nav to angular style

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { ThemeToggleComponent } from '../theme-toggle/theme-toggle.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [ThemeToggleComponent],
+  imports: [ThemeToggleComponent,CommonModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
+  isMobileMenuOpen = false;
 
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
 }
